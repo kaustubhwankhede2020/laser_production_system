d05357bd23aee4fb5c26eba1dc66229bb5c0f1b5
Krishna Wankhede <kkwankhede2020@gmail.com>
2020-12-25 19:58:33 +0530

    first commit

---
+++ laser_production_app/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Daily_update(models.Model):
+    main_id = models.AutoField(primary_key=True)
+    date = models.CharField(max_length=15)
+    shift = models.CharField(max_length=5)
+    employee_number = models.CharField(max_length=10)
+    component_type = models.CharField(max_length=25)
+    component_lk_number = models.CharField(max_length=25)
+    component_lot_number = models.CharField(max_length=5)
+    component_quantity = models.CharField(max_length=5)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
+    
